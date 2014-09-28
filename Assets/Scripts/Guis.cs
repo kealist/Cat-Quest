8260dfdd8270095206ecb64fe6ff75af55749b5a
Joshua Shireman <kealist@gmail.com>
2014-09-27 21:51:23 -0500

    Cat graphics updates

--- Assets/Scripts/Guis.cs
+++ Assets/Scripts/Guis.cs
@@ -5,13 +5,17 @@ using System.Collections;
 public class Guis : MonoBehaviour {
 	void OnGUI() {
 		GUI.color = Color.black;
+		GUI.backgroundColor = Color.white;
 		StatScript playerStats = GameObject.Find("Player").GetComponent<StatScript>();
 		GUILayout.Button("Explore");
-		GUILayout.Label ("Health: " + playerStats.totalHP);
-		GUILayout.Label ("Magic: " + playerStats.totalMP);
-		GUILayout.Label ("Strength: " + playerStats.totalSTR);
-		GUILayout.Label ("Vitality: " + playerStats.totalVIT);
-		GUILayout.Label ("Intelligence: " + playerStats.totalINT);
-
+		GUILayout.Button("Run away");
+		GUILayout.Button("Attack");
+		GUILayout.BeginArea(new Rect(0, Screen.height/4, 300, 300));
+		GUILayout.Label("Health: " + playerStats.totalHP);
+		GUILayout.Label("Magic: " + playerStats.totalMP);
+		GUILayout.Label("Strength: " + playerStats.totalSTR);
+		GUILayout.Label("Vitality: " + playerStats.totalVIT);
+		GUILayout.Label("Intelligence: " + playerStats.totalINT);
+		GUILayout.EndArea();
 	}
 }
